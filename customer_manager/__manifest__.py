f534d9c02ace23e715253a416452354b16bd4ba1
RudwanYahia <156898914+RudwanYahia@users.noreply.github.com>
2026-03-24 16:53:53 +0300

    Add manifest for Customer Manager module

---
+++ customer_manager/__manifest__.py
@@ -0,0 +1,9 @@
+{
+    'name': 'Customer Manager',
+    'version': '1.0',
+    'summary': 'Manage customers',
+    'author': 'Rudwan Yahia',
+    'depends': ['base'],
+    'data': [],
+    'installable': True,
+}
